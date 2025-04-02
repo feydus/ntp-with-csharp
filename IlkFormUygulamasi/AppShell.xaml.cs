2af4952d9359eba7edc5d41553ae2da8a2bf0bd4
Feyduş Kıymet Kocahan <feyduskocahann@gmail.com>
2025-04-02 06:46:42 +0300

    *1
    İlk form uygulaması eklendi

---
+++ IlkFormUygulamasi/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace IlkFormUygulamasi;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ IlkFormUygulamasi/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿namespace IlkFormUygulamasi;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+    void ToplaClicked(object sender, EventArgs e)
+    {
+        // TextBox'lardan değer al
+        int sayi1 = Convert.ToInt32(sayi1Entry.Text);
+        int sayi2 = Convert.ToInt32(sayi2Entry.Text);
+
+        // Toplama işlemi
+        int toplam = sayi1 + sayi2;
+
+        // Sonucu Label'a yaz
+        sonucLabel.Text = $"Toplam: {toplam}";
+    }
+}
